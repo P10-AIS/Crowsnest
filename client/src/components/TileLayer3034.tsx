d901bda7032b0e329a8f5e3f59651effcd9ec50b
William Woldum <41169797+williamwoldum@users.noreply.github.com>
2025-11-16 01:44:53 +0000

    Added nicer tile provider

--- client/src/components/TileLayer3034.tsx
+++ client/src/components/TileLayer3034.tsx
@@ -9,13 +9,18 @@ function TileLayer3034() {
     useEffect(() => {
         const wmsOptions: L.WMSOptions = {
             crs: CRS_3034,
-            layers: 'SRTM30-Colored',
+            layers: 'world',
             format: 'image/png',
             transparent: true,
-            version: '1.3.0',
+            version: '1.1.1',
+            attribution: '© Omniscale',
         };
-        const layer = L.tileLayer.wms('http://ows.mundialis.de/services/service?', wmsOptions);
 
+        // const wmsUrl = 'https://maps.omniscale.net/v2/trajviz-1f42b4b3/style.grayscale/map?';
+        const wmsUrl = 'https://maps.omniscale.net/v2/trajviz-1f42b4b3/style.default/map?';
+
+
+        const layer = L.tileLayer.wms(wmsUrl, wmsOptions);
 
         map.addLayer(layer);
 
@@ -26,6 +31,5 @@ function TileLayer3034() {
 
     return null;
 }
-
 
 export default TileLayer3034;
